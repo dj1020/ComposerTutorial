94ee416e80f684109da69122a7dd8608125c0d9e
Ken Lin <kenlin@kkbox.com>
2017-09-09 12:33:59 +0800

    加入 CartItem 計算 total amount

--- CartExample.php
+++ CartExample.php
@@ -6,4 +6,10 @@ $id = '閃亮亮Cart';
 $store = new \Cart\Storage\SessionStore();
 $cart =  new \Cart\Cart($id, $store);
 
-var_dump($cart->toArray());
+$cart->add(new \Cart\CartItem(['name'=> '冰與火之歌第1季', 'quantity' => 1, 'price' => 100]));
+$cart->add(new \Cart\CartItem(['name'=> '冰與火之歌第2季', 'quantity' => 1, 'price' => 200]));
+$cart->add(new \Cart\CartItem(['name'=> '冰與火之歌第3季', 'quantity' => 1, 'price' => 300]));
+
+print_r($cart->toArray()['items']);
+echo "---------------------------" . PHP_EOL;
+print_r('一共 ' . $cart->total() . ' 元');
