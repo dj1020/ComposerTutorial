6ba5cf4e80ca8c867c05f3dfdabf2ef20c9085bd
Ken Lin <kenlin@kkbox.com>
2017-09-12 17:34:36 +0800

    Add MyCartExample.php

---
+++ src/MyCartExample.php
@@ -0,0 +1,15 @@
+<?php
+
+
+
+$id = 'MyCart Example';
+$store = new \MyCart\Storage\SessionStore();
+$cart =  new \MyCart\Cart($id, $store);
+
+$cart->add(new \MyCart\CartItem(['name'=> '冰與火之歌第1季', 'quantity' => 1, 'price' => 100]));
+$cart->add(new \MyCart\CartItem(['name'=> '冰與火之歌第2季', 'quantity' => 1, 'price' => 200]));
+$cart->add(new \MyCart\CartItem(['name'=> '冰與火之歌第3季', 'quantity' => 1, 'price' => 300]));
+
+print_r($cart->toArray()['items']);
+echo "---------------------------" . PHP_EOL;
+print_r('一共 ' . $cart->total() . ' 元');
