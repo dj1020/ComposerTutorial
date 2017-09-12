22a46a496d409cb4e74b633c2337d63d5472169c
Ken Lin <kenlin@kkbox.com>
2017-09-12 18:01:36 +0800

    加入 itemList() helper function in MyCartExample.php

--- MyCartExample.php
+++ MyCartExample.php
@@ -10,6 +10,18 @@ $cart->add(new \MyCart\CartItem(['name'=> '冰與火之歌第1季', 'quantity' =
 $cart->add(new \MyCart\CartItem(['name'=> '冰與火之歌第2季', 'quantity' => 1, 'price' => 200]));
 $cart->add(new \MyCart\CartItem(['name'=> '冰與火之歌第3季', 'quantity' => 1, 'price' => 300]));
 
-print_r($cart->toArray()['items']);
+print_r(itemList($cart));
 echo "---------------------------" . PHP_EOL;
 print_r('一共 ' . $cart->total() . ' 元');
+
+function itemList($cart) {
+    if (! $cart instanceof \MyCart\Cart) {
+        throw new Exception("Not a cart object");
+    }
+
+    if (! isset($cart->toArray()['items'])) {
+        throw new Exception("Something wrong");
+    }
+
+    return $cart->toArray()['items'];
+}
